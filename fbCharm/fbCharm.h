99da3e29af8c32a6ce64ee994b079d91cb20526c
pcjbird <pcjbird@hotmail.com>
2018-12-15 23:26:31 +0800

    首次提交

--- fbCharm/fbCharm.h
+++ fbCharm/fbCharm.h
@@ -5,6 +5,12 @@
 //  Created by pcjbird on 2018/12/15.
 //  Copyright © 2018 Zero Status. All rights reserved.
 //
+//  框架名称:fbCharm
+//  框架功能:iOS 上利用 fallback 机制为不同语言的文字 (script) 设定字体，从而使得文本混排更为优雅。
+//  修改记录:
+//     pcjbird    2018-12-15  Version:1.0.0 Build:201812150001
+//                            1.首次发布SDK版本
+//
 
 #import <UIKit/UIKit.h>
 
