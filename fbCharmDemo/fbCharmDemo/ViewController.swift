ef8212f269f88aca564dc490dfcdc93378c97607
pcjbird <pcjbird@hotmail.com>
2018-12-16 16:26:02 +0800

    更新演示动画，更新demo

--- fbCharmDemo/fbCharmDemo/ViewController.swift
+++ fbCharmDemo/fbCharmDemo/ViewController.swift
@@ -60,32 +60,33 @@ class ViewController: UIViewController {
         let text = "これは日本語文章と Roman Text の混植文章です。美しいヒラギノと San Francisco で日本語とローマ字を書きます。System Font のフォントメトリクスには独自の調整が入っています。\n\n这是一段中文简体。\n這是一段中文繁體。\nうつくしい森で飾られたモーリオ市、\n郊外のぎらぎらひかる草の波。\n祇辻飴葛蛸鯖鰯噌庖箸\n底辺直卿蝕薩化\nABCDEFGHIJKLM\nabcdefghijklm\n1234567890\niClockᴹᴵᴺᴵ\nClockª"
         let fontSize: CGFloat = 18.0
         
-        let bLight = fontweightSwitch.isOn
+        let bHeavy = fontweightSwitch.isOn
         let bParagragh = paragraphSwitch.isOn
         let bKern = kernSwitch.isOn
         let bFallback = fallbackSwitch.isOn
         let bJPFallback = jpSwitch.isOn
         
         let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .justified
         paragraphStyle.minimumLineHeight = fontSize * 1.5
         paragraphStyle.maximumLineHeight = paragraphStyle.minimumLineHeight
         self.label.attributedText = NSMutableAttributedString(string: "")
         if !bFallback {
             if !bParagragh {
-                self.label.font = UIFont(families: [], size: fontSize, weight: bLight ? .light : .regular)
+                self.label.font = UIFont(families: [], size: fontSize, weight: bHeavy ? .heavy : .regular)
                 self.label.text = text
             }
             else{
-                self.label.attributedText = NSMutableAttributedString(text: text, families: [], size: fontSize, weight: bLight ? .light : .regular, kern: bKern ? 4.0 : 0.0, paragraphStyle:paragraphStyle)
+                self.label.attributedText = NSMutableAttributedString(text: text, families: [], size: fontSize, weight: bHeavy ? .heavy : .regular, kern: bKern ? 4.0 : 0.0, paragraphStyle:paragraphStyle)
             }
         }
         else {
             if !bParagragh {
-                self.label.font = UIFont(families: ["Lucida Grande", "Baskerville", "Apple SD Gothic Neo"], size: fontSize, weight: bLight ? .light : .regular)
+                self.label.font = UIFont(families: ["Lucida Grande", "Baskerville", "Apple SD Gothic Neo"], size: fontSize, weight: bHeavy ? .heavy : .regular)
                 self.label.text = text
             }
             else{
-                self.label.attributedText = NSMutableAttributedString(text: text, families: ["Lucida Grande", "Baskerville", "Apple SD Gothic Neo"], size: fontSize, weight: bLight ? .light : .regular, kern: bKern ? 4.0 : 0.0, paragraphStyle:paragraphStyle, fallback: bJPFallback ? "jp" : nil)
+                self.label.attributedText = NSMutableAttributedString(text: text, families: ["Lucida Grande", "Baskerville", "Apple SD Gothic Neo"], size: fontSize, weight: bHeavy ? .heavy : .regular, kern: bKern ? 4.0 : 0.0, paragraphStyle:paragraphStyle, fallback: bJPFallback ? "jp" : nil)
                 
             }
         }
